c5923890d863413239e103f38577500be5ddee4f
wwx_global <1070386252@qq.com>
2018-07-19 15:12:24 +0800

    修改Ad类

--- src/main/java/org/imooc/bean/Ad.java
+++ src/main/java/org/imooc/bean/Ad.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @JsonInclude(Include.NON_NULL)
 public class Ad extends BaseBean {
-    private Long id;
+    private Long id; //为防止从数据库中取数据时为空，导致错误，故采用封装类型，此时数据库如为空，则赋值为null
     private String title;
     private String imgFileName;
     private String link;
